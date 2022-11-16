d030d434a6415899aa6c16cbba34eb8fc0f0f00b
user <oksi.1213@mail.ru>
2022-11-17 00:48:28 +1000

    A43

--- A43/Program.cs
+++ A43/Program.cs
@@ -16,7 +16,8 @@ int[] askNumbers()
     return ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 }
 
-void printResult(int[] array) {
+void printResult(int[] array) 
+{
     double x = ((double) array[2] - array[0]) / ((double) array[1] - array[3]);
     double y = array[3] * x + array[2];
     WriteLine($"b1 = {array[0]}, k1 = {array[1]}, b2 = {array[2]}, k2 = {array[3]} -> ({x}; {y})");
